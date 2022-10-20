a37899648bb9be46b69f0a5a4ffed92d65c6bb23
Sergey Lokshin <chamie@ya.ru>
2022-10-20 04:25:42 +0300

    Add JSDoc comments to storage service

--- src/services/storageService.ts
+++ src/services/storageService.ts
@@ -1,15 +1,31 @@
 import { BillDto } from "../types/billModel";
 import { ContactModel } from "../types/contactModel";
 
+/**
+ * Loads bills array from "bills" LocalStorage key
+ * @returns Bills array or empty array if no such key
+ */
 export const loadBills = (): BillDto[] =>
     (loadLSDataByName<BillDto[]>("bills") || []);
 
+/**
+ * Saves a bills array to "bills" LocalStorage key
+ * @param bills Bills array to save
+ */
 export const saveBills = (bills: BillDto[]): void =>
     saveLSDataByName<BillDto[]>("bills", bills);
 
+/**
+ * Loads contacts from "contacts" LocalStorage key
+ * @returns Contacts array or empty array if no such key.
+ */
 export const loadContacts = (): ContactModel[] =>
     loadLSDataByName("contacts") || [];
 
+/**
+ * Serializes and saves contacts array to LocalStorage under a "contacts" key.
+ * @param contacts contacts array
+ */
 export const saveContacts = (contacts: readonly ContactModel[]) =>
     saveLSDataByName("contacts", contacts);
 
