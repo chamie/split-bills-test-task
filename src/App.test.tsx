de310f3cbb6f99a16eb56042a929318aca33f0ea
Sergey Lokshin <chamie@ya.ru>
2022-10-25 04:38:37 +0300

    Add tests for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-xtest('renders learn react link', () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(screen.getByText("Bills")).toBeInTheDocument();
+describe('App component', () => {
+  ['Contacts', 'Bills', 'Export/Import'].forEach(menuItemName => {
+    it(`should render ${menuItemName} link`, () => {
+      // Act
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      );
+      // Assert
+      expect(screen.getAllByText(menuItemName).length).toBeTruthy();
+    });
+  });
 });
